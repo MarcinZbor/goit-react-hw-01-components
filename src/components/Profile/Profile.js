353fd9281d27e558505de05086b54ac556634e21
MarcinZbor <marcinzb@gazeta.pl>
2023-05-09 12:01:20 +0200

    addinPropTypes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -34,4 +34,17 @@ function Profile({name, tag, location, avatar, stats}) {
   );
 }
 
+
+Profile.propTypes = {
+name: PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired,
+location: PropTypes.string.isRequired,
+avatar: PropTypes.string.isRequired,
+stats: PropTypes.shape ({
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+})
+}
+
 export default Profile;
