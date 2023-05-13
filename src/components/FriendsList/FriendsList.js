6f93df97f3ca8fb2cbf28ba995736208b942c145
MarcinZbor <marcinzb@gazeta.pl>
2023-05-13 13:41:57 +0200

    fixedPropTypes

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -15,7 +15,7 @@ function FriendsList({ friends }) {
         avatar={avatar}
       />
       )}
-      
+
     </ul>
   );
 }
--- src/components/FriendsList/FriendsListItem/FriendsListItem.js
+++ src/components/FriendsList/FriendsListItem/FriendsListItem.js
@@ -2,27 +2,18 @@ import React from 'react';
 import css from '../FriendsListItem/FriendsListItem.module.css';
 import PropTypes from 'prop-types';
 
-export const FriendsListItem = ({ avatar, name, isOnline}) => {
+export const FriendsListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span
-        className={isOnline ? css.statusOnline : css.statusOffline}
-      ></span>
-      <img
-        className={css.avatar}
-        src={avatar}
-        alt={name}
-        width="48"
-      />
+      <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
+      <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
   );
 };
 
 FriendsListItem.propTypes = {
-    friend: PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.string.isRequired,
-    }).isRequired,
-  };
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
